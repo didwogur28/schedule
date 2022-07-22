54ed6de0581fb8be195c027a49194951e7eb5880
yangjaehyuk <didwogur28@naver.com>
2022-07-22 17:46:44 +0900

    # feat : 관리자, 사용자 로그인 성공 시 페이지 다른곳으로 이동되게 구분 개발 중

--- src/main/java/com/schedule/security/CustomLoginSuccessHandler.java
+++ src/main/java/com/schedule/security/CustomLoginSuccessHandler.java
@@ -1,20 +1,35 @@
 package com.schedule.security;
 
+import com.schedule.login.vo.LoginVO;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Map;
 
 public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        response.sendRedirect("/schedule/goMain");
-    }
 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        HttpSession session = request.getSession();
+
+        Map<String, Object> authDetails = (Map<String, Object>) authentication.getDetails();
+        session.setAttribute("USER", authDetails.get("USER"));
+
+//        String roles = loginVO.getRoles();
+//        if ("ROLE_ADMIN".equals(roles)) {
+//
+//            response.sendRedirect("/schedule/goMain");
+//        } else {
+//
+//        }
+    }
 }
